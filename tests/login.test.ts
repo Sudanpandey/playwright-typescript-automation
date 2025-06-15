860e7066f33cc0faad160850e1c33088c0060f4d
Sudanpandey <lewin.pandey01@gmail.com>
2025-06-15 15:34:38 +0545

    folder structure and refactor code

---
+++ tests/login.test.ts
@@ -0,0 +1,25 @@
+import { test, expect, chromium } from '@playwright/test';
+
+test('Login Test Demo:',async () =>{
+    const browser = await chromium.launch({
+        headless: false
+    });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    
+
+await page.goto("https://ecommerce-playground.lambdatest.io/");
+await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]");
+await page.click("text=Login");
+await page.fill("input[name ='email']","lewin.pandey01+1@gmail.com")
+await page.fill("input[name='password']","Test@1234");
+await page.click("input[value='Login']");
+await page.waitForTimeout(5000);
+
+
+const newContext = await browser.newContext();
+const newPage = await newContext.newPage();
+await newPage.goto("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
+await newPage.waitForTimeout(5000);
+
+});
